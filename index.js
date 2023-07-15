2816b61f8031acdd93ea7026b8cedf232ba731a2
Abdullah Muhammad Siam <abdullahms838@gmail.com>
2023-07-15 14:13:07 +0600

    find chef data

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const app = express();
+const cors = require('cors')
 const port = process.env.PORT || 5000;
 
 const chefs = require('./data/chefs.json');
+
+app.use(cors());
 
 app.get('/', (req, res) => {
     res.send('chef is chocking')
@@ -10,6 +13,12 @@ app.get('/', (req, res) => {
 
 app.get('/chefs', (req, res) => {
     res.send(chefs);
+})
+
+app.get('/chefs/:id', (req, res) => {
+    const id = req.params.id;
+    const selectedChef = chefs.find(chef => chef.unique_id === id);
+    res.send(selectedChef);
 })
 
 app.listen(port, () => {
